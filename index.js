915ccfdd7b24b1f5d277f9cfc7c30277a7ce6fe9
Michael Boroski <mjboroski@gmail.com>
2018-07-08 22:39:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,33 @@ const logDriversByHometown = function(drivers, location){
       console.log(driver.name);
   }});
 };
+
+const driversByRevenue = function(drivers, location){
+  drivers.forEach(function(driver){
+    if (driver.hometown === location){
+      console.log(driver.name);
+  }});
+};
+
+const driversByName = function(drivers, location){
+  drivers.forEach(function(driver){
+    if (driver.hometown === location){
+      console.log(driver.name);
+  }});
+};
+
+
+const totalRevenue  = function(drivers, location){
+  drivers.forEach(function(driver){
+    if (driver.hometown === location){
+      console.log(driver.name);
+  }});
+};
+
+
+const averageRevenue  = function(drivers, location){
+  drivers.forEach(function(driver){
+    if (driver.hometown === location){
+      console.log(driver.name);
+  }});
+};
