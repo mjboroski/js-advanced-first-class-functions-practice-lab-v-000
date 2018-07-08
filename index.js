c0408941604d9205d3a32d05ad1d162ebc8df64b
Michael Boroski <mjboroski@gmail.com>
2018-07-08 22:34:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const logDriverNames = function(drivers){
-  drivers.forEach(function(driver){console.log driver.name})
-}
+  drivers.forEach(function(driver){
+    console.log driver.name;
+  });
+};
