6da52d51bbd169a907e600375bfafe5fa4dce8fc
Michael Boroski <mjboroski@gmail.com>
2018-07-08 22:38:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const logDriverNames = function(drivers){
 
 const logDriversByHometown = function(drivers, location){
   drivers.forEach(function(driver){
-    console.log(driver.name) if (driver.hometown === location);
-  });
+    if (driver.hometown === location){
+      console.log(driver.name);
+  }});
 };
